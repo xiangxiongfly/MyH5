c95fc2c70022c4bc6abe2881fc6090f37a3b09ac
xiangxiongfly <xiangxiongfly@163.com>
2023-03-31 14:31:09 +0800

    放大镜效果

---
+++ 放大镜/data.js
@@ -0,0 +1,17 @@
+// data.js
+let imageUrls = [
+  { b: "../img/phone/b1.png", s: "../img/phone/s1.png" },
+  { b: "../img/phone/b2.png", s: "../img/phone/s2.png" },
+  { b: "../img/phone/b3.png", s: "../img/phone/s3.png" },
+  { b: "../img/phone/b1.png", s: "../img/phone/s1.png" },
+  { b: "../img/phone/b2.png", s: "../img/phone/s2.png" },
+  { b: "../img/phone/b3.png", s: "../img/phone/s3.png" },
+  { b: "../img/phone/b1.png", s: "../img/phone/s1.png" },
+  { b: "../img/phone/b2.png", s: "../img/phone/s2.png" },
+  { b: "../img/phone/b3.png", s: "../img/phone/s3.png" },
+  { b: "../img/phone/b1.png", s: "../img/phone/s1.png" },
+  { b: "../img/phone/b2.png", s: "../img/phone/s2.png" },
+  { b: "../img/phone/b3.png", s: "../img/phone/s3.png" },
+  { b: "../img/phone/b1.png", s: "../img/phone/s1.png" },
+  { b: "../img/phone/b2.png", s: "../img/phone/s2.png" },
+];
